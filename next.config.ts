bda8fa7382bc12b0c22a26a7a2dd3aedeb92dfce
buildwithjaym <jaymmaruji@gmail.com>
2026-08-28 07:58:41 -0700

    added configuration in next config

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,13 @@
-import type {
-  NextConfig,
-} from "next";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname:
-          "oslmujgbzavguwvhppeu.supabase.co",
+        hostname: "oslmujgbzavguwvhppeu.supabase.co",
         port: "",
-        pathname:
-          "/storage/v1/object/public/**",
+        pathname: "/storage/v1/object/public/business-media/**",
       },
     ],
   },
